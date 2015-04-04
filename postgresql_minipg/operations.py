d05fefcb07ad67b5f97bd623ca76acd5ca1eabba
Hajime Nakagami <nakagami@gmail.com>
2015-04-04 22:09:19 +0900

    fix lookup_cast()

--- postgresql_minipg/operations.py
+++ postgresql_minipg/operations.py
@@ -67,19 +67,23 @@ class DatabaseOperations(BaseDatabaseOperations):
     def deferrable_sql(self):
         return " DEFERRABLE INITIALLY DEFERRED"
 
-    def lookup_cast(self, lookup_type):
+    def lookup_cast(self, lookup_type, internal_type=None):
         lookup = '%s'
 
         # Cast text lookups to text to allow things like filter(x__contains=4)
         if lookup_type in ('iexact', 'contains', 'icontains', 'startswith',
                            'istartswith', 'endswith', 'iendswith', 'regex', 'iregex'):
-            lookup = "%s::text"
+            if internal_type in ('IPAddressField', 'GenericIPAddressField'):
+                lookup = "HOST(%s)"
+            else:
+                lookup = "%s::text"
 
         # Use UPPER(x) for case-insensitive lookups; it's faster.
         if lookup_type in ('iexact', 'icontains', 'istartswith', 'iendswith'):
             lookup = 'UPPER(%s)' % lookup
 
         return lookup
+
 
     def field_cast_sql(self, db_type, internal_type):
         if internal_type == "GenericIPAddressField" or internal_type == "IPAddressField":
