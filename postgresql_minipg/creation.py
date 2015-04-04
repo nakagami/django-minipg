f043aaa10cd1df3cb99c590a90d14f470b98a16d
Hajime Nakagami <nakagami@gmail.com>
2015-04-04 19:22:56 +0900

    move data_types

--- postgresql_minipg/creation.py
+++ postgresql_minipg/creation.py
@@ -4,44 +4,22 @@ try:
     from django.db.backends.creation import BaseDatabaseCreation
 except ImportError:
     from django.db.backends.base.creation import BaseDatabaseCreation
+from django.utils.functional import cached_property
 from django.db.backends.utils import truncate_name
 
-
 class DatabaseCreation(BaseDatabaseCreation):
-    # This dictionary maps Field objects to their associated PostgreSQL column
-    # types, as strings. Column-type strings can contain format strings; they'll
-    # be interpolated against the values of Field.__dict__ before being output.
-    # If a column type is set to None, it won't be included in the output.
-    data_types = {
-        'AutoField': 'serial',
-        'BinaryField': 'bytea',
-        'BooleanField': 'boolean',
-        'CharField': 'varchar(%(max_length)s)',
-        'CommaSeparatedIntegerField': 'varchar(%(max_length)s)',
-        'DateField': 'date',
-        'DateTimeField': 'timestamp with time zone',
-        'DecimalField': 'numeric(%(max_digits)s, %(decimal_places)s)',
-        'FileField': 'varchar(%(max_length)s)',
-        'FilePathField': 'varchar(%(max_length)s)',
-        'FloatField': 'double precision',
-        'IntegerField': 'integer',
-        'BigIntegerField': 'bigint',
-        'IPAddressField': 'inet',
-        'GenericIPAddressField': 'inet',
-        'NullBooleanField': 'boolean',
-        'OneToOneField': 'integer',
-        'PositiveIntegerField': 'integer',
-        'PositiveSmallIntegerField': 'smallint',
-        'SlugField': 'varchar(%(max_length)s)',
-        'SmallIntegerField': 'smallint',
-        'TextField': 'text',
-        'TimeField': 'time',
-    }
 
-    data_type_check_constraints = {
-        'PositiveIntegerField': '"%(column)s" >= 0',
-        'PositiveSmallIntegerField': '"%(column)s" >= 0',
-    }
+    @cached_property
+    def data_types(self):
+        # fallback propery for 1.6 and 1.7
+        from mysql_minipg.base import DatabaseWrapper
+        return DatabaseWrapper.data_types
+
+    @cached_property
+    def data_type_check_constraints(self):
+        # fallback propery for 1.6 and 1.7
+        from mysql_minipg.base import DatabaseWrapper
+        return DatabaseWrapper.data_type_check_constraints
 
     def sql_table_creation_suffix(self):
         test_settings = self.connection.settings_dict['TEST']
