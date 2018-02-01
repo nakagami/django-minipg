59c1fe3cdfa3751bf402b698f54eb8989f48031a
Hajime Nakagami <nakagami@gmail.com>
2018-02-01 17:55:52 +0900

    remove no use lines

--- postgresql_minipg/features.py
+++ postgresql_minipg/features.py
@@ -1,7 +1,4 @@
 from django.db.backends import postgresql
-from django.db.utils import InterfaceError
-from django.utils.functional import cached_property
-
 
 class DatabaseFeatures(postgresql.features.BaseDatabaseFeatures):
     has_native_uuid_field = False
