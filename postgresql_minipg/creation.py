f262ae2539aff2161a2d82fe6da086f4898bb63e
Hajime Nakagami <nakagami@gmail.com>
2018-02-02 21:41:41 +0900

    fix drop test database

--- postgresql_minipg/creation.py
+++ postgresql_minipg/creation.py
@@ -20,7 +20,7 @@ class DatabaseCreation(BaseDatabaseCreation):
         use_ssl = self.connection.settings_dict.get('USE_SSL', False)
         minipg.create_database(parameters['dbname'], host, user, password, port, use_ssl)
 
-    def _destroy_test_db(self, test_database_name, verbosity):
+    def _destroy_test_db(self, test_database_name, verbosity=0):
         host = self.connection.settings_dict.get('HOST')
         user = self.connection.settings_dict.get('USER')
         password = self.connection.settings_dict.get('PASSWORD')
@@ -30,3 +30,48 @@ class DatabaseCreation(BaseDatabaseCreation):
             self.connection.ops.quote_name(test_database_name),
             host, user, password, port, use_ssl
         )
+
+    def _create_test_db(self, verbosity, autoclobber, keepdb=False):
+        """
+        Internal implementation - create the test db tables.
+        """
+        test_database_name = self._get_test_db_name()
+        test_db_params = {
+            'dbname': self.connection.ops.quote_name(test_database_name),
+            'suffix': self.sql_table_creation_suffix(),
+        }
+        # Create the test database and connect to it.
+        with self._nodb_connection.cursor() as cursor:
+            try:
+                self._execute_create_test_db(cursor, test_db_params, keepdb)
+            except Exception as e:
+                # if we want to keep the db, then no need to do any of the below,
+                # just return and skip it all.
+                if keepdb:
+                    return test_database_name
+
+                sys.stderr.write(
+                    "Got an error creating the test database: %s\n" % e)
+                if not autoclobber:
+                    confirm = input(
+                        "Type 'yes' if you would like to try deleting the test "
+                        "database '%s', or 'no' to cancel: " % test_database_name)
+                if autoclobber or confirm == 'yes':
+                    try:
+                        if verbosity >= 1:
+                            print("Destroying old test database for alias %s..." % (
+                                self._get_database_display_str(verbosity, test_database_name),
+                            ))
+                        self._destroy_test_db(test_db_params['dbname'])
+                        self._execute_create_test_db(cursor, test_db_params, keepdb)
+                    except Exception as e:
+                        sys.stderr.write(
+                            "Got an error recreating the test database: %s\n" % e)
+                        sys.exit(2)
+                else:
+                    print("Tests cancelled.")
+                    sys.exit(1)
+
+        return test_database_name
+
+
