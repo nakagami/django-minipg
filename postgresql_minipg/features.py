da3a552f6b62a989e67d583d91414fcde0e6cde4
Hajime Nakagami <nakagami@gmail.com>
2018-02-07 10:03:02 +0900

    support callproc()

--- postgresql_minipg/features.py
+++
@@ -1,8 +0,0 @@
-from django.db.backends.postgresql.features import DatabaseFeatures as BaseFeatures
-
-class DatabaseFeatures(BaseFeatures):
-
-    # callproc is not supported
-    create_test_procedure_without_params_sql = ''
-    create_test_procedure_with_int_param_sql = ''
-    supports_callproc_kwargs = False
