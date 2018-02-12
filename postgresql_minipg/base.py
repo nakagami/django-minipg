2939ca843306462c89bebbab987c7249dfb8cd3e
Hajime Nakagami <nakagami@gmail.com>
2018-02-12 19:43:54 +0900

    modify vendor name

--- postgresql_minipg/base.py
+++ postgresql_minipg/base.py
@@ -31,7 +31,7 @@ from django.db.backends.postgresql.utils import utc_tzinfo_factory             #
 DatabaseFeatures.gis_enabled = False
 
 class DatabaseWrapper(BaseDatabaseWrapper):
-    vendor = 'postgresql'
+    vendor = 'postgresql_minipg'
     display_name = 'PostgreSQL'
     # This dictionary maps Field objects to their associated PostgreSQL column
     # types, as strings. Column-type strings can contain format strings; they'll
