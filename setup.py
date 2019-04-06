fd0e72e286b9d51468e89b8369d43ddcf4dca8b2
Hajime Nakagami <nakagami@gmail.com>
2019-04-06 14:29:20 +0900

    revision number up

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ classifiers = [
 ]
 
 setup(name='django-minipg', 
-        version='0.3.4',
+        version='0.3.5',
         description='Django database backend for minipg',
         long_description=open('README.rst').read(),
         url='https://github.com/nakagami/django-minipg/',
