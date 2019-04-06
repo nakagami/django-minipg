5010af0e3f8d2e6ea045443bcc8e6d3224cbf0fe
Hajime Nakagami <nakagami@gmail.com>
2019-04-06 14:14:14 +0900

    Can't clone database

--- postgresql_minipg/features.py
+++ postgresql_minipg/features.py
@@ -3,3 +3,4 @@ from django.db.backends.postgresql.features import DatabaseFeatures as BaseDatab
 
 class DatabaseFeatures(BaseDatabaseFeatures):
     gis_enabled = False
+    can_clone_databases = False
